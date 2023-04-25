33ad73b870bdc364dc0c90592a50e11b9a94aea0
JonnyMoura <jmfmoura.123@gmail.com>
2023-04-25 17:46:12 +0100

    changes

--- Projeto2_EA/problem_b2.cpp
+++ Projeto2_EA/problem_b2.cpp
@@ -12,6 +12,7 @@ long long int max_profit(vector<int>& prices, int k, int fee, int task) {
 
     for (int i = 1; i < n; i++) {
         dp[i][0] = max(dp[i - 1][0], dp[i - 1][1] - prices[i] * k);
+        
         dp[i][1] = max(dp[i - 1][1], dp[i - 1][0] + prices[i] * k - fee * k);
     }
     if (task == 2 || task ==1) {
@@ -48,6 +49,7 @@ void count_schemes(vector<int> & prices, long long int k, long long int fee) {
     std::vector<std::vector<long long int>> dp(n, std::vector<long long int>(k + 1, 0));
     vector<vector<long long int>> count(n, vector<long long int>(k + 1, 0));
     const int MOD = 1000000007;
+
 
 
     for (int j = 0; j <= k; j++) {
